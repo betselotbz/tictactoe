2aab3a892c3199a2b7b2821309a87aa8d277fa1b
Betselot Bezaebeh <betselotsahilu@gmail.com>
2023-08-18 08:03:48 -0500

    added winning condtiong

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const boardGame = [
   ["", "", ""],
 ];
 let currentPlayer = "X";
+let score = 0;
 
 function playAction(row, col) {
   //Validation state before action
@@ -27,13 +28,46 @@ function playAction(row, col) {
   //Validation state after action
   /* need to reflect new state to UI
    */
-  winningCondition();
+  //winningCondition();
   drawCondition();
 
   // update UI
 }
-function winningCondition() {}
-
+function winningCondition() {
+  if (
+    (boardGame[0][0], boardGame[(1, 1)], boardGame[2][2]) ||
+    (boardGame[0][2], boardGame[1][1], boardGame[2][0]) === currentPlayer
+  ) {
+    score = score + 1;
+    alert("WIN");
+  } else {
+    score = score - 1;
+    alert("LOST"); //Diagonal Winning
+  }
+  if (
+    (boardGame[0][0], boardGame[(1, 0)], boardGame[2][0]) ||
+    (boardGame[0][1], boardGame[1][1], boardGame[2][1]) ||
+    (boardGame[0][2], boardGame[1][2], boardGame[2][2]) === currentPlayer
+  ) {
+    score = score + 1;
+    alert("WIN");
+  } else {
+    score = score - 1;
+    alert("LOST"); //Vertical Winning
+  }
+  if (
+    (boardGame[0][0], boardGame[(0, 1)], boardGame[0][2]) ||
+    (boardGame[1][0], boardGame[1][1], boardGame[1][2]) ||
+    (boardGame[2][0], boardGame[2][1], boardGame[2][2]) === currentPlayer
+  ) {
+    score = score + 1;
+    alert("WIN");
+  } else {
+    score = score - 1;
+    alert("LOST"); //Horizontal Winning
+  }
+}
+function trackScore() {}
 function drawCondition() {}
 function reset() {}
 
