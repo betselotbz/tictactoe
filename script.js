9c979f4fe6dd9a745882d763849bfe1932af8355
Betselot Bezaebeh <betselotsahilu@gmail.com>
2023-08-19 08:15:10 -0500

    added custom attribute in html and js

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ const boardGame = [
 ];
 let currentPlayer = "X";
 let score = 0;
+const gridBoxes = document.querySelectorAll(".grid-box");
 
 function playAction(row, col) {
   //Validation state before action
   /*check if cell is empty 
     prevent moves in occupied cells*/
-
+    const row = grids.dataset.row;
+    const col = grids.dataset.col;
   let cell = boardGame[row][col];
   if (cell !== "") {
     alert("OCCUPIED");
     return;
   }
-
   // update table
   boardGame[row][col] = currentPlayer;
   //Player Turns
@@ -25,24 +26,33 @@ function playAction(row, col) {
   } else {
     currentPlayer = "X";
   }
+  
   //Validation state after action
   /* need to reflect new state to UI
    */
-  let play = document.querySelector(".grid-box");
-  document.addEventListener("click", function (event) {
-    event.preventDefault();
-    play.textContent = "X";
-    play.style.fontSize = "90px";
-    playAction([row], [col]);
-  });
-  drawCondition();
+
+  //drawCondition();
 
   // update UI
 }
+
+gridBoxes.forEach(function (grids) {
+  grids.addEventListener("click", function (event) {
+    event.preventDefault();
+    grids.textContent = "X";
+    grids.style.fontSize = "90px";
+    playAction(row, col);
+    // winningCondition();
+  });
+});
 function winningCondition() {
   if (
-    (boardGame[0][0], boardGame[(1, 1)], boardGame[2][2]) ||
-    (boardGame[0][2], boardGame[1][1], boardGame[2][0]) === currentPlayer
+    boardGame[0][0],
+    boardGame[1],
+    [1],
+    boardGame[2][2] || boardGame[0][2],
+    boardGame[1][1],
+    boardGame[2][0] = currentPlayer
   ) {
     score = score + 1;
     alert("WIN");
@@ -50,10 +60,13 @@ function winningCondition() {
     score = score - 1;
     alert("LOST"); //Diagonal Winning
   }
-  if (
-    (boardGame[0][0], boardGame[(1, 0)], boardGame[2][0]) ||
-    (boardGame[0][1], boardGame[1][1], boardGame[2][1]) ||
-    (boardGame[0][2], boardGame[1][2], boardGame[2][2]) === currentPlayer
+  if(boardGame[0][0],
+    boardGame[1][0],
+    boardGame[2][0] || boardGame[0][1],
+    boardGame[1][1],
+    boardGame[2][1] || boardGame[0][2],
+    boardGame[1][2],
+    boardGame[2][2] = currentPlayer
   ) {
     score = score + 1;
     alert("WIN");
@@ -62,9 +75,13 @@ function winningCondition() {
     alert("LOST"); //Vertical Winning
   }
   if (
-    (boardGame[0][0], boardGame[(0, 1)], boardGame[0][2]) ||
-    (boardGame[1][0], boardGame[1][1], boardGame[1][2]) ||
-    (boardGame[2][0], boardGame[2][1], boardGame[2][2]) === currentPlayer
+    boardGame[0][0],
+    boardGame[0][1],
+    boardGame[0][2] || boardGame[1][0],
+    boardGame[1][1],
+    boardGame[1][2] || boardGame[2][0],
+    boardGame[2][1],
+    boardGame[2][2] = currentPlayer
   ) {
     score = score + 1;
     alert("WIN");
@@ -73,8 +90,8 @@ function winningCondition() {
     alert("LOST"); //Horizontal Winning
   }
 }
-function trackScore() {}
-function drawCondition() {}
-function reset() {}
+// function trackScore() {}
+// function drawCondition() {}
+// function reset() {}
 
 playAction(1, 1);
